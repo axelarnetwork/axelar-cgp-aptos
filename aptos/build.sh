0f4287e9830f34e9a1e554c331d18274fe3878c7
npty <78221556+npty@users.noreply.github.com>
2022-12-11 19:43:12 +0700

    fix: check for aptos binary before compile (#1)
    * fix: check for aptos binary before compile
    
    * chore: bump to 1.0.1

--- aptos/build.sh
+++ aptos/build.sh
@@ -1,4 +1,9 @@
 # !/bin/bash
 
-aptos move compile --save-metadata --package-dir aptos/modules/axelar
-aptos move compile --save-metadata --package-dir aptos/modules/test
+if command -v aptos &>/dev/null; then
+  aptos move compile --save-metadata --package-dir aptos/modules/axelar
+  aptos move compile --save-metadata --package-dir aptos/modules/test
+else
+  echo "aptos not found. skip building modules."
+  exit
+fi
